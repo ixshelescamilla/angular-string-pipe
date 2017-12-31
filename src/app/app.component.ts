d5b134d3194a15eb4204e8c73c0df29d5b8e71d3
ixshel escamilla <iescamilla@esri.com>
2017-12-31 00:10:34 -0800

    styles and descriptions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  example: string = '';
+  example: string = 'Enter something here';
 }
