9f98d88c23152773cabf8e71fe9442267803b97f
ixshel escamilla <iescamilla@esri.com>
2017-12-30 22:54:11 -0800

    string pipes and example working

---
+++ src/app/pipes/string.pipe.ts
@@ -0,0 +1,54 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'string'
+})
+export class StringPipe implements PipeTransform {
+
+  transform(value: string, option: any, args?: any): string {
+
+    let newStr: string = '';
+
+    switch (option) {
+      case 'reverse':
+        for (let i = value.length - 1; i >= 0; i--) {
+          newStr += value.charAt(i);
+        }
+        break;
+
+      case 'firstCap':
+        if (value) {
+          newStr = value.charAt(0).toUpperCase() + value.slice(1);
+        }
+        break;
+
+      case 'capAllWords':
+        if (value) {
+          newStr = value.replace(/\b\w/g, first => first.toLocaleUpperCase());
+        }
+        break;
+
+      case 'noSpaces':
+        if (value) {
+          newStr = value.replace(/ /g, '');
+        }
+        break;
+
+      case 'truncate':
+        if (value) {
+          const limit = args.length > 0 ? parseInt(args[0], 10) : 10;
+          const trail = args.length > 1 ? args[1] : '...';
+          newStr = value.length > limit ? value.substring(0, limit) + trail : value;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+
+
+    return newStr;
+  }
+
+}
